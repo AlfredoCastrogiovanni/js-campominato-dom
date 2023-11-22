a63c1ce7d6cc4d1f40d6098bb9ee07dde5e5eac9
AlfredoCastrogiovanni <castro.alfry@gmail.com>
2023-11-22 11:52:40 +0100

    implementazione bombe e bg al click

--- js/index.js
+++ js/index.js
@@ -20,14 +20,46 @@ function createSquare(wrapper,times) {
         newSquare.style.height = `calc(100% / ${Math.sqrt(times)})`;
 
         const squareContent = document.createElement("span");
-            squareContent.textContent = i;
-            newSquare.append(squareContent);
+        squareContent.textContent = i;
+        newSquare.append(squareContent);
+
+        const bombList = generateBomb(times);
 
         newSquare.addEventListener("click", function() {
-            this.classList.add("bg-blue");
-            console.log(this.textContent);
+            // this.classList.add("bg-blue");
+            // console.log(this.textContent);
+
+            if(bombList.includes(parseInt(this.textContent))) {
+                this.classList.add("bg-red");
+                gameOver = false;
+            } else {
+                this.classList.add("bg-blue");
+            }
         });
         
         wrapper.append(newSquare);
     }
+}
+
+function generateUniqueRandomNumber(numberList, min, max) {
+    if(numberList.length > max - min + 1) {
+        console.log("Numeri massimi raggiunti");
+        return ;
+    }
+
+    let generatedNumber;
+    do {
+        generatedNumber = Math.floor((Math.random() * (max - min)) + min);
+    } while(numberList.includes(generatedNumber)); 
+
+    return generatedNumber;
+}
+
+function generateBomb(bombNumber) {
+    const bomb = [];
+    for(let i=0; i < 16; i++) {
+        bomb[i] = generateUniqueRandomNumber(bomb, 1, bombNumber);
+    }
+
+    return bomb;
 }
